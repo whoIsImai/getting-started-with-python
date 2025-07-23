58fd6bdba03eb5864175298b1ee446ed6f870107
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-07-23 20:18:23 +0200

    add another function to add numbers

--- Basics/functions.py
+++ Basics/functions.py
@@ -1,4 +1,9 @@
-def hello_world():
-    print("Hello World From Function!")
+def hello_world() -> str:
+   return print("Hello World From Function!")
 
 hello_world()
+
+def add_numbers(num1: int, num2: int) -> int:
+    return print(num1 + num2)
+
+add_numbers(2,10)
