db59deba0974ae8ccbfa34b1e28269da2edc68f4
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-07-25 19:37:03 +0200

    comment out

--- string.py
+++ string.py
@@ -5,7 +5,7 @@ paragraph = "This is a sample paragraph that will be used to demonstrate string 
 
 #print(len(paragraph))
 
-if("Python" in paragraph):
-    print("Python is present in the paragraph.")
-else:
-    print("Python is not present in the paragraph.")
+# if("Python" in paragraph):
+#     print("Python is present in the paragraph.")
+# else:
+#     print("Python is not present in the paragraph.")
