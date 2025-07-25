d355db4e3967b6930de3837982a3b709362e5d54
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-07-25 19:36:14 +0200

    else

--- string.py
+++ string.py
@@ -4,3 +4,8 @@ paragraph = "This is a sample paragraph that will be used to demonstrate string 
 #     print(x)
 
 #print(len(paragraph))
+
+if("Python" in paragraph):
+    print("Python is present in the paragraph.")
+else:
+    print("Python is not present in the paragraph.")
