d0477c0c7805fc6c23f47d3696464b87a6cf9e42
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-07-26 10:39:45 +0200

    last index

--- List.py
+++ List.py
@@ -1,3 +1,7 @@
 my_list = ["apple", "banana", "cherry", "mango", "orange", "grape", "kiwi", "pear"]
 
 #print(my_list[2:5]) # Output: ['cherry', 'mango', 'orange']
+
+# last_index = len(my_list) -1
+
+# print(my_list[last_index])
