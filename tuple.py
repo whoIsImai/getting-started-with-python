d3704b114b4ccbe70a565fd7781bc6922b176c2e
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-07-28 19:51:22 +0200

    comment out

--- tuple.py
+++ tuple.py
@@ -2,8 +2,8 @@ my_tuple = ("apple", "banana", "cherry", "mango", "orange", "grape", "kiwi", "pe
 
 #print(my_tuple)
 
-for index in range(len(my_tuple)):
-    print(my_tuple[index])
+# for index in range(len(my_tuple)):
+#     print(my_tuple[index])
 
 i = 0
 while i < len(my_tuple):
