cb8e40633d5d31e3e1fb51c4882d9adcb49859bb
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-07-29 19:51:49 +0200

    get list and values

--- dictionary.py
+++ dictionary.py
@@ -3,5 +3,9 @@ my_dic = {
     "age": 30,
     "city": "Randburg",
 }
-name = my_dic["name"]
-print(f"Name: {name}")
+# name = my_dic["name"]
+# print(f"Name: {name}")
+
+keys = my_dic.keys()
+values = my_dic.values()
+print(f"Keys: {keys}- Values: {values}")
