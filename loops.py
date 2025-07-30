123717a8d4ef0075e5714a26b2323f7438a5d4a8
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-07-30 19:44:47 +0200

    commented out code

--- loops.py
+++ loops.py
@@ -1,9 +1,9 @@
-i = 0
-while i < 10:
-    print(f"Value: {i}")
-    i+=1
-    if i == 5:
-        print(f"Breaking at {i}")
-        continue
-else:
-    print("Loop completed without break")
+# i = 0
+# while i < 10:
+#     print(f"Value: {i}")
+#     i+=1
+#     if i == 5:
+#         print(f"Breaking at {i}")
+#         continue
+# else:
+#     print("Loop completed without break")
