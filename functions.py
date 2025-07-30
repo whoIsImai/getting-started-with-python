facc64833debba94c458841ac9e801798d74fbf8
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-07-30 19:57:19 +0200

    comment out

--- functions.py
+++ functions.py
@@ -1,12 +1,12 @@
-def hello_world() -> str:
-   return print("Hello World From Function!")
+# def hello_world() -> str:
+#    return print("Hello World From Function!")
 
-hello_world()
+# hello_world()
 
-def add_numbers(num1: int, num2: int) -> int:
-    return print(num1 + num2)
+# def add_numbers(num1: int, num2: int) -> int:
+#     return print(num1 + num2)
 
-add_numbers(2,10)
+# add_numbers(2,10)
 
 def User(*user) -> str:
     return print(f"name: {user["name"]}, age: {user["age"]}, country: {user["country"]}")
