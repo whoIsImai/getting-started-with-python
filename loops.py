933347a3f9389dbd0139a16cb7ae0bbd02967082
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-07-30 19:49:29 +0200

    for loop

--- loops.py
+++ loops.py
@@ -11,6 +11,10 @@
 list_1 = [1, 2, 3, 4, 5]
 list_2 = [6, 7, 8, 9, 10]
 
-for i in range(len(list_1)):
-    for j in range(len(list_2)):
-        print(f"Pair: {list_1[i]}, {list_2[j]}")
+# for i in range(len(list_1)):
+#     for j in range(len(list_2)):
+#         print(f"Pair: {list_1[i]}, {list_2[j]}")
+
+for x in list_1:
+    for y in list_2:
+        print(f"Pair: {x}, {y}")    
