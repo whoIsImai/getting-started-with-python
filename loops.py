2d118764c3753d73364d0965403dd54aa9d93ede
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-07-30 19:44:12 +0200

    else statement

--- loops.py
+++ loops.py
@@ -1,7 +1,9 @@
 i = 0
 while i < 10:
     print(f"Value: {i}")
-    if i == 5:
-        print("Reached halfway!")
-        break
     i+=1
+    if i == 5:
+        print(f"Breaking at {i}")
+        continue
+else:
+    print("Loop completed without break")
