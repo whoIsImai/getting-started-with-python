e3f92336a6879fc9cce7c8943209a141c47f4e93
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-07-31 19:44:25 +0200

    say hi

--- oop.py
+++ oop.py
@@ -2,8 +2,11 @@ class person:
     def __init__(self, name: str, age:int):
         self.name = name
         self.age = age
-    def __str__(self):
-        return f"Hi, i'm {self.name} and i am {self.age} old"
+    # def __str__(self):
+    #     return f"Hi, i'm {self.name} and i am {self.age} old"
+    
+    def greet(self):
+        return f"Hello, my name is {self.name} and I am {self.age} years old."
     
 p1 = person("John", 30)
 print(p1)
