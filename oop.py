f50379045795a23d80aea46a7adfa7f9c2710298
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-07-31 19:48:04 +0200

    comment out

--- oop.py
+++ oop.py
@@ -1,13 +1,13 @@
-class person:
-    def __init__(self, name: str, age:int):
-        self.name = name
-        self.age = age
-    # def __str__(self):
-    #     return f"Hi, i'm {self.name} and i am {self.age} old"
+# class person:
+#     def __init__(self, name: str, age:int):
+#         self.name = name
+#         self.age = age
+#     # def __str__(self):
+#     #     return f"Hi, i'm {self.name} and i am {self.age} old"
     
-    def greet(self):
-        return f"Hello, my name is {self.name} and I am {self.age} years old."
+#     def greet(self):
+#         return f"Hello, my name is {self.name} and I am {self.age} years old."
     
-p1 = person("John", 30)
-print(p1.greet())
+# p1 = person("John", 30)
+# print(p1.greet())
         
