e2197f788131bde485a58c9a04cc8a366f1031e4
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-08-01 18:52:17 +0200

    date and comment

--- Variables.py
+++ Variables.py
@@ -7,21 +7,26 @@
 
 # print(X+ " "+Y)
 
-num1 : int
-num2 : int
-num1 = 5
-num2 = 10
-num3 : int = num1 + num2
+# num1 : int
+# num2 : int
+# num1 = 5
+# num2 = 10
+# num3 : int = num1 + num2
 
-print(num3)
+# print(num3)
 
-name : str
-surname : str
-name = "John"
-surname = "Doe"
+# name : str
+# surname : str
+# name = "John"
+# surname = "Doe"
 
-full_name= f"{name} {surname}"
-print(full_name)
+# full_name= f"{name} {surname}"
+# print(full_name)
+
+import datetime
+
+current_date = datetime.datetime.now()
+print(current_date)
 
 
 
