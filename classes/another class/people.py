d0c07392862e3fb938bd420f8952a95571156725
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-08-02 19:43:00 +0200

    call course method

--- classes/another class/people.py
+++ classes/another class/people.py
@@ -8,5 +8,9 @@ class teacher(Person):
     def __init__(self, name, age, role):
         super().__init__(name,age,role)
 
-student1 = student("Imai", 21, "Student").Intro()
-teacher1 = teacher("Cos", 40,"Teacher").Intro()
+student1 = student("Imai", 21, "Student")
+
+teacher1 = teacher("Cos", 40,"Teacher")
+
+student1.Intro()
+student1.getCourse()
