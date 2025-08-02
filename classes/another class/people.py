afcb5ee8baae9995e6455dd16a33d7a68f712597
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-08-02 19:37:25 +0200

    inheritence

--- classes/another class/people.py
+++ classes/another class/people.py
@@ -1,2 +1,12 @@
 from person import Person
 
+class student(Person):
+    def __init__(self, name, age, role):
+        super().__init__(name, age, role)
+    
+class teacher(Person):
+    def __init__(self, name, age, role):
+        super().__init__(name,age,role)
+
+student1 = student("Imai", 21, "Student")
+teacher1 = teacher("Cos", 40,"Teacher")
