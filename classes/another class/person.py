db73062e4063758fb6ebd026703e18d84db25a91
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-08-02 19:41:43 +0200

    add course

--- classes/another class/person.py
+++ classes/another class/person.py
@@ -6,3 +6,7 @@ class Person():
 
     def Intro(self):
         return print(f"Hi, i'm {self.name} {self.age} old. I'm a {self.role}")
+    
+    def getCourse(self):
+        course = input("Enter your course: ")
+        return print(f"{self.name} is doin {course}")
