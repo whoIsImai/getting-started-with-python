2c354c74eb5faace71b77cb9987a4ebec9e733c7
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-08-12 20:05:09 +0200

    comment code

--- pandas/import.py
+++ pandas/import.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
-data_set = {
-    "cars" : ["Toyota","Camry", "Honda", "Civic"],
-    "year" : [2020, 2021, 2022, 2023],
-    "price" : [20000, 22000, 25000, 270]
-}
-df = pd.DataFrame(data_set, index = ["1st", "2nd", "3rd", "4th"])
-# series = pd.Series(data_set)
+# data_set = {
+#     "cars" : ["Toyota","Camry", "Honda", "Civic"],
+#     "year" : [2020, 2021, 2022, 2023],
+#     "price" : [20000, 22000, 25000, 270]
+# }
+# df = pd.DataFrame(data_set, index = ["1st", "2nd", "3rd", "4th"])
+# # series = pd.Series(data_set)
 
-# print("### series ###")
-# print(series)
-print("### DataFrame ###")
-print(df)
-print("### DataFrame with index ###")
-print(df.loc["1st":"2nd", ["cars", "year"]])
+# # print("### series ###")
+# # print(series)
+# print("### DataFrame ###")
+# print(df)
+# print("### DataFrame with index ###")
+# print(df.loc["1st":"2nd", ["cars", "year"]])
