a89dcb37edefb5c620d5b5943f26c9683c5310db
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-08-26 20:39:09 +0200

    replace null values and not remove empty ones

--- clean.py
+++ clean.py
@@ -2,6 +2,9 @@ import pandas as pd
 
 df = pd.read_csv("data.csv")
 
-new_df = df.dropna(inplace = True)
+# new_df = df.dropna(inplace = True)
 
+# print(new_df.to_string())
+
+new_df = df.fillna(111, inplace = True)
 print(new_df.to_string())
