2bd4b889d3aa40e87d2e51b0538691cea44b1acb
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-08-26 20:46:33 +0200

    get the mean of the calories and replace null values with it

--- clean.py
+++ clean.py
@@ -2,8 +2,12 @@ import pandas as pd
 
 df = pd.read_csv("data.csv")
 
-# new_df = df.dropna(inplace = True)
+# # new_df = df.dropna(inplace = True)
 
-# print(new_df.to_string())
+# # print(new_df.to_string())
 
-new_df = df.fillna({"Calories": 130}, inplace = True)
+# new_df = df.fillna({"Calories": 130}, inplace = True)
+
+v = df["Calories"].mean()
+
+df.fillna({"Calories": v}, inplace=True)
